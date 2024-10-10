d3a8c1ded5b18fa9b2a321cd6a8fc042bd8107c1
Sara Helba <29603156+sjhelba@users.noreply.github.com>
2024-10-10 10:01:06 +0200

    add Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import * as React from "react";
+import * as React from "react"
+import CssBaseline from '@mui/material/CssBaseline';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-const App = () => <div>App</div>;
+const App = () => (
+  <div>
+    <CssBaseline />
+  </div>
+)
 
-export { App };
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
-import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import * as React from "react"
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-const root = createRoot(document.getElementById('root') as HTMLElement);
+const root = createRoot(document.getElementById('root') as HTMLElement)
 
-root.render(<App />);
+root.render(<App />)
