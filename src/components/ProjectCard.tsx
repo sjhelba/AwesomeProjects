38be6da245caa9bba619e22af2dc68a7a7d734d9
Sara Helba <29603156+sjhelba@users.noreply.github.com>
2024-10-12 18:53:14 +0200

    prettify cards

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Box, Card, CardActions, CardContent, IconButton, Typography } from "@mui/material"
+import { Box, Card, CardActions, IconButton, Typography } from "@mui/material"
 import { GithubProjectData } from "../utils/constantsAndTypes"
 import DeleteIcon from '@mui/icons-material/Delete'
 import StarIcon from '@mui/icons-material/Star'
@@ -12,21 +12,36 @@ interface ProjectCardProps {
 
 export const ProjectCard = ({projectData, deleteCallback, color}: ProjectCardProps) => {
 
+  const deleteButtonClickHandler: React.MouseEventHandler<HTMLButtonElement> = event => {
+    event.preventDefault()
+    deleteCallback()
+  }
+
   return (
-    <Card data-testid="proj-card" raised style={{backgroundColor: color}}>
-      <CardContent>
-      <CardActions>
-        <IconButton aria-label="delete" size="small" data-testid="delete-btn" onClick={deleteCallback}><DeleteIcon /></IconButton>
+    <Card data-testid="proj-card" raised className="card" style={{backgroundColor: color}}>
+      <a
+          href={projectData.url}
+          target="_blank"
+          rel="noreferrer"
+          data-testid="card-action-area"
+      >
+        <Typography component="h2" variant="h5" className="card-title">{projectData.name}</Typography>
+        <CardActions className="card-actions">
+          <IconButton
+            aria-label="delete"
+            size="small"
+            data-testid="delete-btn"
+            onClick={deleteButtonClickHandler}
+          >
+            <DeleteIcon fontSize="small"/>
+          </IconButton>
         </CardActions>
-      <a href={projectData.url} target="_blank" rel="noreferrer" data-testid="card-action-area">
-        <Typography variant="h2">{projectData.name}</Typography>
-        <Box sx={{display: 'flex'}}>
+        <Box className="card-rating flex">
           {[...Array(projectData.rating)].map((_, index) => (
             <StarIcon key={index} data-testid="star" />
           ))}
         </Box>
       </a>
-      </CardContent>
     </Card>
   )
 }
